be6697f0af764713b2997c8508e9da29a882f62c
g18000689 <REmi_0809>
2019-09-16 14:08:16 +0200

    exo1

---
+++ tp1.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+    <body>
+<?php
+echo '<strong> Voici mon premier programme PHP </strong><br/>' . "\n";
+echo 'C\'est pas mal non ? <br/>';
+$var1 = 6;
+$var2 = 1.3;
+$var3 = 'Variable 3';
+echo "$var1 + $var2";
+echo $var1 + $var2;
+echo $var1 + $var3;
+?>
+</body>
+</head>
+</html>
