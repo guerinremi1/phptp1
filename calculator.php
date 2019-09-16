1b8c93c0ca743ffc50bef74ac00c333aff104d21
g18000689 <REmi_0809>
2019-09-16 16:39:07 +0200

    exo1

---
+++ calculator.php
@@ -0,0 +1,21 @@
+<?php
+//dazdazd//
+include 'index.php';
+start_page("calculatrice");
+$operateurs = '*+-/';
+echo '
+    <form action = "calcul.php" method = "get">
+    <input type = "text" name = "op1" ><br>
+    <input type = "text" name = "op2" ><br>';
+for($cpt=0;$cpt<=3;++$cpt)
+{
+    echo '<input ';
+    if($cpt==0)
+    {
+        echo'checked = "checked"';
+    }
+    echo'type="submit" name="action" value="'.$operateurs[$cpt].'"/>'.$operateurs[$cpt].'<br/>
+        '."\n";}
+echo '</form>';
+end_page();
+?>
