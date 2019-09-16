92977cf77c5e7cbc31be5de79d06bd548d34a8e1
g18000689 <REmi_0809>
2019-09-16 15:33:29 +0200

    exo1

--- tp1exo2.php
+++ tp1exo2.php
@@ -2,7 +2,7 @@
 function start_page($title)
 {
     echo ' <!DOCTYPE html> <html
-lang="fr"><head><title>' . PHP_EOL . $title . '</title></head><body>' . PHP_EOL
+lang="fr"><head><title>' . PHP_EOL . $title . '</title></head><body>'. PHP_EOL
     ;
 };
 
